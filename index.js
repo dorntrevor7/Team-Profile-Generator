31a4e0315d083a89b3f2a3b660898bbee751cd94
dorntrevor7 <dorntrevor7@gmail.com>
2022-10-12 23:36:36 -0700

    switch case with team option

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ const fs = require("fs");
 inquirer
   .prompt([
     {
-      type: "input",
-      name: "teamName",
+      type: "checkbox",
+      name: "teamType",
+      choices: ["Manager", "Engineer", "Intern"],
     },
   ])
   .then(function (data) {
-    console.log(data);
+    // const dataStr = JSON.stringify(data, null, 2);
+    const teamPicked = data.teamType;
+    teamPicked.forEach((team) => {
+      switch (team) {
+        case "Manager":
+          managerAdd();
+          break;
+        case "Engineer":
+          engineerAdd();
+          break;
+        case "Intern":
+          internAdd();
+          break;
+      }
+    });
   })
   .catch(function (err) {
     throw new Error(err.message);
